e8dd37af866279c9a2fbd49be2d025978fc2ca17
shodgin <scotthodgin@gmail.com>
2017-02-13 11:09:10 -0700

    Added Technology Section

--- app/controllers/resumeCtrl.js
+++ app/controllers/resumeCtrl.js
@@ -1,7 +1,18 @@
 app.controller('resumeCtrl', function($scope, ResumeLoader){
-    $scope.personalInfo = { firstName: 'Scott', lastName: 'Hodgin' } ;
     ResumeLoader.fetchResume('content/ScottHodginResume.json')
         .then(function(response) {
             $scope.data = response.data;
         });
+    $scope.getTech = function(catagory) {
+        return $scope.data.technology.filter(function(tech) {return tech.catagory === catagory});
+    };
+    $scope.getTechCatagories = function(){      
+        var catagories = {};
+
+        for (var i in $scope.data.technology){
+            catagories[$scope.data.technology[i].catagory] = true;
+        }
+
+        return Object.keys(catagories);
+    };
 });
